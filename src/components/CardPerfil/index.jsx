19dd3850fef63c817016c2f0b31ca9716cde7818
Antonio Servin <antonio.servin.m11@gmail.com>
2021-08-15 04:40:20 -0500

    feat: Add Material-icon package to import Settings icon on user profile, add modal structure

--- src/components/CardPerfil/index.jsx
+++ src/components/CardPerfil/index.jsx
@@ -2,17 +2,49 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './cardperfil.scss'
 import gear from 'assets/icons/edit_profile.svg'
 import background from 'assets/images/image7.png'
+import { Modal } from 'react-bootstrap';
+import IconButton from '@material-ui/core/IconButton';
+import SettingsIcon from '@material-ui/icons/Settings';
+import { Button } from 'react-bootstrap';
+import { useState } from 'react';
+import ActionBtn from 'components/ActionBtn'
 // import background from 'assets/images/bruno-aguirre-yJBMbQgw9mg-unsplash.jpg'
 
+function MyVerticallyCenteredModal(props) {
+  return (
+    <Modal
+      {...props}
+      size="lg"
+      aria-labelledby="contained-modal-title-vcenter"
+      centered
+    >
+      <Modal.Header className="border-0" closeButton />
+      <Modal.Title id="contained-modal-title-vcenter" className="text-center">
+        Edita tu perfil
+      </Modal.Title>
+      <Modal.Body>
+        
+      </Modal.Body>
+      <Modal.Footer className="justify-content-center w-100">
+        <ActionBtn action="Cerrar" onClick={props.onHide}>Close</ActionBtn>
+      </Modal.Footer>
+    </Modal>
+  );
+}
+
+
 const CardPerfil = ({avatar, user_first_name, user_last_name, user_username, user_position, user_dominant_foot, user_date_joined}) => {
-  
+  const [modalShow, setModalShow] = useState(false);
   return (
     <div className="card-perfil text-center mx-auto d-flex flex-column justify-content-end" style={{backgroundImage: `url(${background})`}}>
       <div className="data-container px-4">
         <div className="head d-flex flex-column align-items-center">
           <div className="avatar mt" style={{backgroundImage: `url(${avatar})`}}></div>
           <div className="edit-profile-icon">
-            <a href=""><img src={gear} alt="edit_profile" /></a>
+            {/* <a href=""><img src={gear} alt="edit_profile" /></a> */}
+            <IconButton onClick={() => setModalShow(true)}>
+              <SettingsIcon/>
+            </IconButton>
           </div>
           <h5>{user_first_name} {user_last_name}</h5>
           <p>@ {user_username}</p>
@@ -25,6 +57,11 @@ const CardPerfil = ({avatar, user_first_name, user_last_name, user_username, use
         </div>
         <p className="date-joined-p">Jugando fútbol desde: <strong>{user_date_joined}</strong></p>
       </div>
+
+      <MyVerticallyCenteredModal
+        show={modalShow}
+        onHide={() => setModalShow(false)}
+      />
     </div>
   )
 }
--- src/components/UserMatches/index.jsx
+++ src/components/UserMatches/index.jsx
@@ -3,6 +3,7 @@ import './usermatches.scss'
 import MatchResume from "components/MatchResume"
 
 const UserMatches = ({matches, fields, num_matches}) => {
+  console.log(matches)
   return (
     <div className="user-matches-container text-center py-4 px-2 mx-auto">
       <div className="d-flex flex-row justify-content-between align-items-center p-3">
@@ -18,7 +19,7 @@ const UserMatches = ({matches, fields, num_matches}) => {
         {
           matches.map((match, index) => (
             
-            <MatchResume key={index.toString()} date={match.date} field_name={match.field.name} match_type={match.field.football_type}/>
+            <MatchResume key={index.toString()} date={match.date} field_name={match.field.name} match_type={match.field.football_type} /* category={match.field.category} *//>
           ))
         }
       </div>
