0ce7846446464f0aa9c0b829312ec8d746327f6b
Aaron Mendoza <aaronmg@Mac-mini-de-Aaron.local>
2021-08-28 12:21:46 -0500

    ADD Match page

--- src/components/DetalleCancha/DetalleCancha.jsx
+++ src/components/DetalleCancha/DetalleCancha.jsx
@@ -1,6 +1,6 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './detallecancha.scss'
-// import { useState } from 'react';
+import { useState , useEffect } from 'react';
 import cancha from 'assets/images/vienna-reyes-Zs_o1IjVPt4-unsplash.jpg'
 import regaderas from 'assets/icons/regaderas.svg'
 import arbitraje from 'assets/icons/arbitraje.svg'
@@ -11,90 +11,79 @@ import location from 'assets/icons/location.svg'
 import calendar from 'assets/icons/calendar.svg'
 import time from 'assets/icons/time.svg'
 import genre from 'assets/icons/genre.svg'
-import { Card } from 'react-bootstrap';
 import UnirseBtn from '../ActionBtn';
+import Divider from '@material-ui/core/Divider';
+import Btn from 'components/Buttons/CallActionBtn'
 
 
-const DetalleCancha = () => {
+
+
+const DetalleCancha = (props) => {
+
+  const {imgField,nameField,servicesField,typeField, priceField, directionField, dateMatch, timeMatch, categoryMatch} = props
   
   // const [showA, setShowA] = useState(true);
   // const toggleShowA = () => setShowA(!showA);
+  const services = ["Estacionamiento","Regadera",
+    "Bebedero"
+  ]
   
   return (
-    <Card className="border-light rounded overflow-auto">
-      <Card.Img variant="top" src={cancha} alt="liga_española_sauces" />
-      <Card.Body>
-        <Card.Title className="fs-3 fw-bolder text-dark">Liga española - Los Sauces</Card.Title>
-        <div className="d-flex flex-wrap justify-content-around align-items-start my-4">
-          <div className="d-flex flex-column flex-wrap justify-content-center align-items-center icon-container">
-            <img className="match-icon" src={arbitraje} alt="arbitraje" />
-            <p className="icon-description mt-2">Arbitraje</p>
-          </div>
-          <div className="d-flex flex-column flex-wrap justify-content-center align-items-center icon-container">
-            <img className="match-icon" src={estacionamiento} alt="estacionamiento" />
-            <p className="icon-description mt-2">Estacionamiento</p>
-          </div>
-          <div className="d-flex flex-column flex-wrap justify-content-center align-items-center icon-container">
-            <img className="match-icon" src={regaderas} alt="regaderas" />
-            <p className="icon-description mt-2">Regaderas</p>
-          </div>
-          <div className="d-flex flex-column flex-wrap justify-content-center align-items-center icon-container">
-            <img className="match-icon" src={bebederos} alt="bebederos" />
-            <p className="icon-description mt-2">Bebederos</p>
-          </div>
-          <div className="d-flex flex-column flex-wrap justify-content-center align-items-center icon-container">
-            <img className="match-icon" src={techada} alt="techada" />
-            <p className="icon-description mt-2">Techada</p>
-          </div>
+    <div className="d-flex flex-column card-partido">
+      <div class=" img-cancha-partido ">
+        <img src={imgField}/>
+      </div>
+      <div className="card-content">
+      <h1 className="text-center fs-1 fw-bold field-title mt-3">{nameField}</h1>
+        <div className="d-flex justify-content-around flex-wrap mt-5">
+          {/* {services.map(item=>(<p className="p-services mx-1">{item}</p>))} */}
+          {servicesField}
         </div>
-        <div className="d-flex flex-row justify-content-around align-items-center">
-          <div>
-            <h2 className="match-type my-1 ">11 vs 11</h2>
-          </div>
-          
+        <div className="d-flex justify-content-around justify-content-md-around align-items-center mt-3 mb-3">
+          <h2 className="create-type fw-bold">{typeField}</h2>
           <div className= "d-flex flex-row justify-content-center align-items-center bg-dark text-warning price-container">
-            <h2 className="my-1 mx-1 match-price">$100</h2>
-            <span className="my-1 mx-2">Precio /<br/>Jugador</span>
+              <h2 className="my-1 mx-1 ">{priceField}</h2>
+              <span className="my-1 mx-2 fw-light fs-6">Precio /<br/>Jugador</span>
           </div>
         </div>
-        <hr/>
-        <div className="location d-flex flex-row justify-content-start align-items-center">
-          <img className="match-icon" src={location} alt="ubicacion" />
-          <p className="py-3 ps-3 m-0">Av. Juan Diego S/N, Tejocote, 64870 Cuautitlán, Méx.</p>
+        <Divider className="mb-4" variant="middle"/>
+        <div className="d-flex justify-content-around align-items-center mt-4 mb-4">
+          <img src={location}/>
+          <p className="mb-0 address">{directionField}</p>
         </div>
-        <hr/>
+        <Divider className="mb-4" variant="middle"/>
         <div>
-          <div className="date-container my-4 d-flex justify-content-between align-items-center">
+          <div className="date-container my-4 d-flex justify-content-around align-items-center">
             <div className="d-flex justify-content-start align-items-center">
               <img className="match-icon" src={calendar} alt="fecha"/>
               <h5 className="my-0 ms-3">Fecha:</h5>
             </div>
             <div>
-              <p className="my-0">27/07/2021</p>
+              <p className="my-0">{dateMatch}</p>
             </div>
           </div>
-          <div className="time-container my-4 d-flex justify-content-between align-items-center">
+          <div className="time-container my-4 d-flex justify-content-around align-items-center">
             <div className="d-flex justify-content-start align-items-center">
               <img className="match-icon" src={time} alt="hora"/>
               <h5 className="my-0 ms-3">Horario:</h5>
             </div>
             <div>
-              <p className="my-0">10:20 am</p>
+              <p className="my-0">{timeMatch}</p>
             </div>
           </div>
-          <div className="genre-container my-4 d-flex justify-content-between align-items-center">
+          <div className="genre-container my-4 d-flex justify-content-around align-items-center">
             <div className="d-flex justify-content-start align-items-center">
               <img className="match-icon" src={genre} alt="categoria"/>
               <h5 className="my-0 ms-3">Categoria:</h5>
             </div>
             <div>
-              <p className="my-0">Femenil</p>
+              <p className="my-0 category">{categoryMatch}</p>
             </div>
           </div>
         </div>
-        <UnirseBtn action="Unirse"/>
-      </Card.Body>
-    </Card>
+      </div>
+      <Btn className="mb-3" text="Unirse"/>
+    </div>
     
   )
 }
--- src/components/Equipos/Equipos.jsx
+++ src/components/Equipos/Equipos.jsx
@@ -2,7 +2,36 @@ import './equipos.scss'
 import JugadorEquipo from '../JugadorEquipo'
 import UnirseBtn from '../ActionBtn'
 
-const Equipos = () => {
+const Equipos = (props) => {
+  const {typeMatch } = props
+  const players = []
+
+  if (typeMatch === 1){
+    for (let i = 1 ; i<=10 ; i++){
+      players.push( <div className="d-flex flex-row justify-content-around flex-nowrap">
+      <JugadorEquipo team="black"/>
+      <JugadorEquipo team="white"/>
+    </div>)
+    }
+  }else if (typeMatch === 2){
+    for (let i = 1 ; i<=8 ; i++){
+      players.push( <div className="d-flex flex-row justify-content-around flex-nowrap">
+      <JugadorEquipo team="black"/>
+      <JugadorEquipo team="white"/>
+    </div>)
+    }
+  }else if (typeMatch === 3){
+    for (let i = 1 ; i<=14 ; i++){
+      players.push( <div className="d-flex flex-row justify-content-around flex-nowrap">
+      <JugadorEquipo team="black"/>
+      <JugadorEquipo team="white"/>
+    </div>)
+    }
+  }
+
+  
+
+
   return (
     <div className="match-container p-3">
       <h5 className="fs-5 fw-bolder my-4">1 lugar disponible</h5>
@@ -13,62 +42,7 @@ const Equipos = () => {
         <span>Equipo blanco</span>
       </div>
       <div className="teams-container my-4">
-        <div className="d-flex flex-row justify-content-around flex-nowrap">
-          <JugadorEquipo team="black"/>
-          <JugadorEquipo team="white"/>
-        </div>
-        <div className="d-flex flex-row justify-content-around flex-nowrap">
-          <JugadorEquipo team="black"/>
-          <JugadorEquipo team="white"/>
-        </div>
-        <div className="d-flex flex-row justify-content-around flex-nowrap">
-          <JugadorEquipo team="black"/>
-          <JugadorEquipo team="white"/>
-        </div>
-        <div className="d-flex flex-row justify-content-around flex-nowrap">
-          <JugadorEquipo team="black"/>
-          <JugadorEquipo team="white"/>
-        </div>
-        <div className="d-flex flex-row justify-content-around flex-nowrap">
-          <JugadorEquipo team="black"/>
-          <JugadorEquipo team="white"/>
-        </div>
-        <div className="d-flex flex-row justify-content-around flex-nowrap">
-          <JugadorEquipo team="black"/>
-          <JugadorEquipo team="white"/>
-        </div>
-        <div className="d-flex flex-row justify-content-around flex-nowrap">
-          <JugadorEquipo team="black"/>
-          <JugadorEquipo team="white"/>
-        </div>
-        <div className="d-flex flex-row justify-content-around flex-nowrap">
-          <JugadorEquipo team="black"/>
-          <JugadorEquipo team="white"/>
-        </div>
-        <div className="d-flex flex-row justify-content-around flex-nowrap">
-          <JugadorEquipo team="black"/>
-          <JugadorEquipo team="white"/>
-        </div>
-        <div className="d-flex flex-row justify-content-around flex-nowrap">
-          <JugadorEquipo team="black"/>
-          <JugadorEquipo team="white"/>
-        </div>
-        <div className="d-flex flex-row justify-content-around flex-nowrap">
-          <JugadorEquipo team="black"/>
-          <JugadorEquipo team="white"/>
-        </div>
-        <div className="d-flex flex-row justify-content-around flex-nowrap">
-          <JugadorEquipo team="black"/>
-          <JugadorEquipo team="white"/>
-        </div>
-        <div className="d-flex flex-row justify-content-around flex-nowrap">
-          <JugadorEquipo team="black"/>
-          <JugadorEquipo team="white"/>
-        </div>
-        <div className="d-flex flex-row justify-content-around flex-nowrap">
-          <JugadorEquipo team="black"/>
-          <JugadorEquipo team="white"/>
-        </div>
+        {players}
       </div>
       <UnirseBtn action="Unirse" />
     </div>
