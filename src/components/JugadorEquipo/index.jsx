338dbc25de3cc288a0fb03f14848d12e3f693d8c
Antonio Servin <antonio.servin.m11@gmail.com>
2021-08-30 23:38:23 -0500

    feat: edit styles of players list into game view

--- src/components/JugadorEquipo/index.jsx
+++ src/components/JugadorEquipo/index.jsx
@@ -6,7 +6,7 @@ const JugadorEquipo = ({player_data}) => {
   
   if (Object.keys(player_data).length === 0) {
     return (
-      <div className="d-flex flex-column justify-content-center align-items-center jugador">
+      <div className="d-flex flex-column justify-content-center align-items-center jugador mb-3">
         <div className="user-img" style={{backgroundImage: `url(${user_noPicURL})`}}></div>
         <h5 className="mt-1 mb-0">Únete</h5> 
       </div>
@@ -14,7 +14,7 @@ const JugadorEquipo = ({player_data}) => {
   }else{
     
     return (
-      <div className="d-flex flex-column justify-content-center align-items-center jugador">
+      <div className="d-flex flex-column justify-content-center align-items-center jugador mb-3">
         <div className="user-img" style={{backgroundImage: `url(${user_pic_URL})`}}></div>
         <h5 className="mt-1 mb-0">{player_data.user_data.username}</h5>
         <p>{player_data.position}</p>
