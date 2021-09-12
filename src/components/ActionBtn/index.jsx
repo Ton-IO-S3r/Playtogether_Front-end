54ecf5813dd6886f011310274765c08d2835e448
Antonio Servin <antonio.servin.m11@gmail.com>
2021-09-12 18:41:19 -0500

    feat: add prop into ActionBtn to set onclick event function

--- src/components/ActionBtn/index.jsx
+++ src/components/ActionBtn/index.jsx
@@ -1,9 +1,16 @@
 import './actionbtn.scss'
-const ActionBtn = ({action, btn_type, btn_context, btn_width, btn_disable, btn_mt}) => {
+const ActionBtn = ({action, btn_type, btn_context, btn_width, btn_disable, btn_mt, clic_func}) => {
   
   return (
     <div className="text-center w-100">
-      <button type={btn_type} className={`action-btn align-self-center mt-${btn_mt} btn ml-auto mr-auto ${btn_context} btn-w-${btn_width}`} disabled={btn_disable}>{action}</button>
+      <button 
+        type={btn_type} 
+        className={`action-btn align-self-center mt-${btn_mt} btn ml-auto mr-auto ${btn_context} btn-w-${btn_width}`} 
+        disabled={btn_disable}
+        onClick={clic_func}
+      >
+        {action}
+      </button>
     </div>
   )
 }
