853a9799aae5e9185358d540a2dafc596475f6d6
Antonio Servin <antonio.servin.m11@gmail.com>
2021-09-12 18:12:55 -0500

    feat: edit ActionBtn component to accept different props to style the basic button

--- src/components/ActionBtn/index.jsx
+++ src/components/ActionBtn/index.jsx
@@ -1,11 +1,20 @@
 import './actionbtn.scss'
-const ActionBtn = ({action, btn_type, btn_disable}) => {
+const ActionBtn = ({action, btn_type, btn_context, btn_width, btn_disable, btn_mt}) => {
   
   return (
     <div className="text-center w-100">
-      <button type={btn_type} className="action-btn align-self-center mt-4 btn ml-auto mr-auto" disabled={btn_disable}>{action}</button>
+      <button type={btn_type} className={`action-btn align-self-center mt-${btn_mt} btn ml-auto mr-auto ${btn_context} btn-w-${btn_width}`} disabled={btn_disable}>{action}</button>
     </div>
   )
+}
+
+ActionBtn.defaultProps ={
+  action: "Aceptar", 
+  btn_type: "button", 
+  btn_context: "accept", 
+  btn_width:"200", 
+  btn_mt:'4',
+  btn_disable:false,
 }
 
 export default ActionBtn
