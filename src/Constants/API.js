3768c66fee7581aed399feb3d8d76ff77daa208f
Aaron Mendoza <aaronmg@Mini-de-Aaron.home>
2021-09-12 21:46:41 -0500

    Add match history

--- src/Constants/API.js
+++ src/Constants/API.js
@@ -1,5 +1,5 @@
-// const API_URL = "https://playtogether-backend.pykode.xyz/api/";
-const API_URL = "http://localhost:8000/api/";
+const API_URL = "https://playtogether-backend.pykode.xyz/api/";
+// const API_URL = "http://localhost:8000/api/";
 
 // const photoAPI = "https://django-playtogether-media.s3.us-east-2.amazonaws.com/"
 // const BACKGROUNDS_URL = "https://django-playtogether-media.s3.us-east-2.amazonaws.com/assets/images/bg/"
