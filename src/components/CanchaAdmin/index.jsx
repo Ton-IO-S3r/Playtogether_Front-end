89fb3261ec5667075785b41adbed1f6681a185fb
Antonio Servin <antonio.servin.m11@gmail.com>
2021-09-15 02:01:01 -0500

    feat: style field data containers depending on active status

--- src/components/CanchaAdmin/index.jsx
+++ src/components/CanchaAdmin/index.jsx
@@ -1,16 +1,24 @@
 import { Card, Col, Row } from 'react-bootstrap'
 import Btn from 'components/Buttons/CallActionBtn'
 import './cancha_admin.scss'
-import { ICONS_URL, photoAPI } from 'Constants/API'
+import { API_URL, ICONS_URL, photoAPI } from 'Constants/API'
 import ToogleBtn from 'components/ToogleBtn/ToogleBtn'
 import ActionBtn from 'components/ActionBtn'
+import { useEffect, useState } from 'react'
+import axios from 'axios'
 
 
-const CanchaAdmin = ({name,cost,address,type,img,services,setModalShow}) => {
+const CanchaAdmin = ({fieldAdminData,name,cost,address,type,img,services,setModalShow, servicesObj, show_field}) => {
+  //Estado para almacenar los servicios
+  const [fieldActive,setFieldActive]=useState(show_field)
   
+  useEffect(()=>{
+    setFieldActive(fieldAdminData.managers.field.show)
+  },[fieldAdminData])
+
   return (
     <>
-      <Card >
+      <Card className="bg-white">
           <Row className="g-0">
             <Col sm={12}>
               <Card.Header className="text-start pt-3">
@@ -24,11 +32,12 @@ const CanchaAdmin = ({name,cost,address,type,img,services,setModalShow}) => {
                 :
                   <>
                     <Card.Text className="activate-field-msg text-success mb-0">
-                      Habilita tu cancha para que los jugadores puedan organizar partidos en ella. 
+                      {fieldActive ? 'Puedes desactivar tu cancha si quieres evitar recibir solicitudes de juego en ella.' : 'Habilita tu cancha para que los jugadores puedan organizar partidos en ella.'}
+                       
                     </Card.Text>
                     <div className="d-flex align-items-center justify-content-center">
                       <span className="mx-2">Off</span>
-                      <ToogleBtn />
+                      <ToogleBtn fieldActive={fieldActive} setFieldActive={setFieldActive}/>
                       <span className="mx-2">On</span>
                     </div>
                   </>
@@ -40,45 +49,61 @@ const CanchaAdmin = ({name,cost,address,type,img,services,setModalShow}) => {
             <Col sm={12}>
               
               
-              <Card.Body className="d-flex flex-column justify-content-start card-body">
+              <Card.Body className={`d-flex flex-column justify-content-start card-body h-auto ${fieldActive ? '':'inactive'}`}>
                   {name === '' || name === null ?
                     <>
                       <h5> Aun no das de alta tu cancha.</h5>
                       <p>Edita tu perfil para agregarla.</p>
-                      <ActionBtn 
-                        action="Editar" 
-                        btn_type="button" 
-                        btn_disable={false} 
-                        aria-controls="collapse-loader"
-                        clic_func = {()=>{setModalShow(true)}}
-                      />
                     </>
                     
                     
                     :
                     
                     <>
-                      <Card.Title className="fw-bold" >{name}</Card.Title>
-                      <Card.Text className="text-truncate">
-                          {address}
-                      </Card.Text>
-                      <Card.Text className="text-truncate fw-bold">
-                          {cost}
-                      </Card.Text>
-                      <Card.Text className="text-truncate">
-                          {type}
-                      </Card.Text>
-                      <Card.Text className="text-truncate">
-                      {services.map(item=>(
-                        <div className="d-flex flex-column flex-wrap justify-content-center align-items-center icon-container">
-                          <img className="match-icon mb-1" src={`${ICONS_URL}${item.toLowerCase()}.svg`} alt="arbitraje" />
-                          <p className="p-services mx-1">{item}</p>
+                      <Card.Title className="fw-bold mb-4" >{name}</Card.Title>
+                      <div className="field-data d-flex flex-wrap justify-content-between align-items-center mb-3">
+                        <Card.Subtitle className="fw-bold">Dirección</Card.Subtitle>
+                        <Card.Text className="text-wrap">
+                            {`${address.street} ${address.street_number}, ${address.town}, ${address.city}`}
+                        </Card.Text>
+
+                      </div>
+                      <div className="field-data d-flex flex-wrap justify-content-between align-items-center mb-3">
+                        <Card.Subtitle className="fw-bold">Precio de renta por partido:</Card.Subtitle>
+                        <Card.Text className="text-truncate fw-bold">
+                            {`$ ${cost}`}
+                        </Card.Text>
+
+                      </div>
+                      <div className="field-data d-flex flex-wrap justify-content-between align-items-center mb-3">
+                        <Card.Subtitle className="fw-bold">Tipo de cancha: </Card.Subtitle>
+                        <Card.Text className="text-truncate">
+                            {type}
+                        </Card.Text>
+
+                      </div>
+                      <div className="field-data d-flex flex-wrap justify-content-between align-items-center mb-3">
+                        <Card.Subtitle className="fw-bold mb-2">Servicios:</Card.Subtitle>
+                        <div className="text-truncate d-flex flex-wrap justify-content-around align-items-center">
+                          {services.map((item,index)=>(
+                            <div key={index.toString()} className="d-flex flex-column flex-wrap justify-content-center align-items-center icon-container">
+                              <img className="match-icon mb-1" src={`${ICONS_URL}${servicesObj[`${item}`].toLowerCase()}${fieldActive ? '' : '_inactive'}.svg`} alt="arbitraje" />
+                              <span className="p-services mx-1">{servicesObj[`${item}`]}</span>
+                            </div>
+                          ))}
                         </div>
-                      ))}
-                      </Card.Text>
+
+                      </div>
+                      {/* <div className={`field-inactive ${fieldActive ? 'd-none':''}`}></div> */}
                     </>
                   }
-                  
+                  <ActionBtn 
+                    action="Editar" 
+                    btn_type="button" 
+                    btn_disable={false} 
+                    aria-controls="collapse-loader"
+                    clic_func = {()=>{setModalShow(true)}}
+                  />
               </Card.Body>
             </Col>
             
